adfa3651488cfe88ae7483f01cf1cf8b6d15364a
Alister <Alister.moral@gmail.com>
2023-05-23 16:59:55 +0200

    finishing library

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Created function that takes user input and store new books into an array.
 Then i created a loops that displays each book on the page
+added "New book" button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read
+added a button on each book’s display to remove the book from the library.
+added a button on each book’s display to change the read status
  
--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 let myLibrary = [];
 
-function Book(title, author, year) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
-  this.year = year;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  let title = prompt("Enter the book title:");
-  let author = prompt("Enter the author:");
-  let year = prompt("Enter the publication year:");
+Book.prototype.toggleReadStatus = function() {
+  this.read = !this.read;
+};
 
-  let newBook = new Book(title, author, year);
-
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-
-  console.log("Book added successfully!");
 }
 
 function displayBooks() {
   let container = document.getElementById("book-container");
-  container.innerHTML = "";
+  removeAllChildNodes(container);
 
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
 
     let bookElement = document.createElement("div");
-    bookElement.className = "book-card"; 
+    bookElement.className = "book-card";
 
     let titleElement = document.createElement("h3");
     titleElement.textContent = book.title;
@@ -34,13 +32,80 @@ function displayBooks() {
     let authorElement = document.createElement("p");
     authorElement.textContent = "Author: " + book.author;
 
-    let yearElement = document.createElement("p");
-    yearElement.textContent = "Year: " + book.year;
+    let pagesElement = document.createElement("p");
+    pagesElement.textContent = "Pages: " + book.pages;
+
+    let readElement = document.createElement("p");
+    readElement.textContent = "Read: " + (book.read ? "Yes" : "No");
+
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute("data-index", i);
+    removeButton.addEventListener("click", removeBook);
+
+    let toggleReadButton = document.createElement("button");
+    toggleReadButton.textContent = "Toggle Read";
+    toggleReadButton.setAttribute("data-index", i);
+    toggleReadButton.addEventListener("click", toggleReadStatus);
 
     bookElement.appendChild(titleElement);
     bookElement.appendChild(authorElement);
-    bookElement.appendChild(yearElement);
+    bookElement.appendChild(pagesElement);
+    bookElement.appendChild(readElement);
+    bookElement.appendChild(removeButton);
+    bookElement.appendChild(toggleReadButton);
 
     container.appendChild(bookElement);
   }
 }
+
+function removeBook(event) {
+  let index = event.target.getAttribute("data-index");
+  myLibrary.splice(index, 1);
+  displayBooks();
+}
+
+function toggleReadStatus(event) {
+  let index = event.target.getAttribute("data-index");
+  let book = myLibrary[index];
+  book.toggleReadStatus();
+  displayBooks();
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+let newBookBtn = document.getElementById("new-book-btn");
+let newBookForm = document.getElementById("new-book-form");
+
+newBookBtn.addEventListener("click", function() {
+  newBookForm.style.display = "block";
+});
+
+let bookForm = document.getElementById("book-form");
+
+bookForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").checked;
+
+  addBookToLibrary(title, author, pages, read);
+
+  displayBooks();
+
+  bookForm.reset();
+
+  newBookForm.style.display = "none";
+});
+
+addBookToLibrary("Book 1", "Author 1", 200, true);
+addBookToLibrary("Book 2", "Author 2", 300, false);
+addBookToLibrary("Book 3", "Author 3", 150, true);
+
+displayBooks();
